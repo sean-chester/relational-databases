deff20f4f520101fe4d6836b6e6e96239a72eaa3
Sean Chester <schester@uvic.ca>
2022-03-02 00:50:25 -0800

    🧪 Add second test case, which is an extension of the first

---
+++ sql/query02.sql
@@ -0,0 +1,7 @@
+-- Retrieve the name of all unique Badges obtained by the user with ID 3 awarded after 2011
+-- together with the first date after 2011 in which that user obtained that badge
+-- 1.1 marks: <4 operators
+-- 1.0 marks: <6 operators
+-- 0.8 marks: correct answer
+
+-- Replace this comment line with the actual query
