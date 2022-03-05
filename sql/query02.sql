5b1e39979b33e60763596b7075ca574221c29f07
Sean Chester <schester@uvic.ca>
2022-03-04 16:54:42 -0800

    🚑 Add sort order to Query 2 to make result unambiguous

--- sql/query02.sql
+++ sql/query02.sql
@@ -1,7 +1,9 @@
--- Retrieve the name of all unique Badges obtained by the user with ID 3 awarded after 2011
--- together with the first date after 2011 in which that user obtained that badge
--- 1.1 marks: <4 operators
--- 1.0 marks: <6 operators
+-- Retrieve the name of all unique Badges obtained by the
+-- user with ID 3 awarded after 2011, together with the
+-- first date after 2011 in which that user obtained that badge.
+-- Order the results by increasing Badge Name.
+-- 1.1 marks: <5 operators
+-- 1.0 marks: <7 operators
 -- 0.8 marks: correct answer
 
 -- Replace this comment line with the actual query
