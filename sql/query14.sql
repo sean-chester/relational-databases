5e47283d40152289d6a3255792241fe52f54ce14
Sean Chester <schester@uvic.ca>
2022-03-05 01:47:48 -0800

    🚑 Correct copy-paste error in score counts for q14

--- sql/query14.sql
+++ sql/query14.sql
@@ -1,8 +1,8 @@
 -- Retrieve the Post that has the highest
 -- score, summed over itself and all its children
--- 1.1 marks: <10 operators
--- 1.0 marks: <14 operators
--- 0.9 marks: <20 operators
+-- 1.1 marks: <8 operators
+-- 1.0 marks: <10 operators
+-- 0.9 marks: <12 operators
 -- 0.8 marks: correct answer
 
 -- Replace this comment line with the actual query
