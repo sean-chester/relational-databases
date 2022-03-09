9cb6f64c2d5ddceee2191097b6d54ddf2a72cfdf
Sean Chester <schester@uvic.ca>
2022-03-08 18:15:29 -0800

    🚑 Fix two errors in q1
    No order was specified for result and the number of ops in the marking scheme was off by one.

--- sql/query01.sql
+++ sql/query01.sql
@@ -1,6 +1,8 @@
--- Retrieve the name and Date of all Badges obtained by the user with ID 3 awarded after 2011
--- 1.1 marks: <3 operators
--- 1.0 marks: <4 operators
+-- Retrieve the name and Date of all Badges obtained by
+-- the user with ID 3 awarded after 2011,
+-- ordered by ascending date
+-- 1.1 marks: <5 operators
+-- 1.0 marks: <6 operators
 -- 0.8 marks: correct answer
 
 -- Replace this comment line with the actual query
