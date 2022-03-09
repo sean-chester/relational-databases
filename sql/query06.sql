2e1e88c4441576bb8d11ad54d9b71b44b1e1379b
Sean Chester <schester@uvic.ca>
2022-03-08 18:38:06 -0800

    🚑 Fix off-by-one error in query 6 scoring (too easy)

--- sql/query06.sql
+++ sql/query06.sql
@@ -1,7 +1,7 @@
 -- Retrieve the display name of all users who have
 -- posted at least one post, ordered ascending
--- 1.1 marks: <6 operators
--- 1.0 marks: <8 operators
+-- 1.1 marks: <5 operators
+-- 1.0 marks: <7 operators
 -- 0.8 marks: correct answer
 
 -- Replace this comment line with the actual query
