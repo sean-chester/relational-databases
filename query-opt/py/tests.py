a06349188013383ef5844809d041b00a210aa675
Sean Chester <schester@uvic.ca>
2022-04-25 20:44:48 -0700

    🧪 Add examinable versions of released test cases

--- query-opt/py/tests.py
+++ query-opt/py/tests.py
@@ -18,11 +18,11 @@ from implement_me import ImplementMe
 class TestCase01(unittest.TestCase):
     def test_insertion(self):
         btree = Index([])
-        key = 42
+        key = 99
 
         expected_output = Index([Node()]*1)
         expected_output.nodes[ 0 ] = Node(\
-            KeySet((42, -1)),\
+            KeySet((99, -1)),\
             PointerSet((0,0,0)))
 
         self.assertEqual( expected_output, ImplementMe.InsertIntoIndex( btree, key ) )
@@ -33,13 +33,13 @@ class TestCase02(unittest.TestCase):
     def test_insertion(self):
         btree = Index([Node()]*1)
         btree.nodes[ 0 ] = Node(\
-            KeySet((42, -1)),\
+            KeySet((99, -1)),\
             PointerSet((0,0,0)))
-        key = 42
+        key = 99
 
         expected_output = Index([Node()]*1)
         expected_output.nodes[ 0 ] = Node(\
-            KeySet((42, -1)),\
+            KeySet((99, -1)),\
             PointerSet((0,0,0)))
 
         self.assertEqual( expected_output, ImplementMe.InsertIntoIndex( btree, key ) )
@@ -50,13 +50,13 @@ class TestCase03(unittest.TestCase):
     def test_insertion(self):
         btree = Index([Node()]*1)
         btree.nodes[ 0 ] = Node(\
-            KeySet((42, -1)),\
+            KeySet((87, -1)),\
             PointerSet((0,0,0)))
-        key = 99
+        key = 66
 
         expected_output = Index([Node()]*1)
         expected_output.nodes[ 0 ] = Node(\
-            KeySet((42, 99)),\
+            KeySet((66, 87)),\
             PointerSet((0,0,0)))
 
         self.assertEqual( expected_output, ImplementMe.InsertIntoIndex( btree, key ) )
@@ -67,19 +67,19 @@ class TestCase04(unittest.TestCase):
     def test_insertion(self):
         btree = Index([Node()]*1)
         btree.nodes[ 0 ] = Node(\
-            KeySet((42, 99)),\
+            KeySet((66, 99)),\
             PointerSet((0,0,0)))
-        key = 7
+        key = 87
 
         expected_output = Index([Node()]*4)
         expected_output.nodes[0] = Node(\
-                KeySet((42, -1)),\
+                KeySet((87, -1)),\
                 PointerSet((1,2,0)))
         expected_output.nodes[1] = Node(\
-                KeySet((7,-1)),\
+                KeySet((66,-1)),\
                 PointerSet((0,0,2)))
         expected_output.nodes[2]=Node(\
-                KeySet((42,99)),\
+                KeySet((87,99)),\
                 PointerSet((0,0,0)))
 
         self.assertEqual( expected_output, ImplementMe.InsertIntoIndex( btree, key ) )
@@ -99,9 +99,9 @@ class TestCase05(unittest.TestCase):
                 KeySet((42,-1)),\
                 PointerSet((0,0,3)))
         btree.nodes[3]=Node(\
-                KeySet((66,99)),\
+                KeySet((66,87)),\
                 PointerSet((0,0,0)))
-        key = 68
+        key = 99
 
         expected_output = Index([Node()]*13)
         expected_output.nodes[0] = Node(\
@@ -111,7 +111,7 @@ class TestCase05(unittest.TestCase):
                 KeySet((42,-1)),\
                 PointerSet((4,5,0)))
         expected_output.nodes[2]=Node(\
-                KeySet((68,-1)),\
+                KeySet((87,-1)),\
                 PointerSet((7,8,0)))
         expected_output.nodes[4]=Node(\
                 KeySet((7,-1)),\
@@ -123,41 +123,190 @@ class TestCase05(unittest.TestCase):
                 KeySet((66,-1)),\
                 PointerSet((0,0,8)))
         expected_output.nodes[8]=Node(\
-                KeySet((68,99)),\
+                KeySet((87,99)),\
                 PointerSet((0,0,0)))
 
         self.assertEqual( expected_output, ImplementMe.InsertIntoIndex( btree, key ) )
 
 
 # Insert into full node with full parent, but does not cause a root split.
+# Note that only the path that should be affected has correct data (testing complexity)
+# Linearisation forces copy of some nodes to new addresses
 class TestCase06(unittest.TestCase):
     def test_insertion(self):
-        btree = Index([]) # not provided
-        key = 87
+        btree = Index([Node()]*13)
+        btree.nodes[0] = Node(\
+                KeySet((7, -1)),\
+                PointerSet((1,2,0)))
+        btree.nodes[2]=Node(\
+                KeySet((27,66)),\
+                PointerSet((7,8,9)))
+        btree.nodes[6]=Node(\
+                KeySet((11,11)),\
+                PointerSet((0,0,90))) # Dummy data for test
+        btree.nodes[7]=Node(\
+                KeySet((7,9)),\
+                PointerSet((0,0,8)))
+        btree.nodes[8]=Node(\
+                KeySet((27,-1)),\
+                PointerSet((0,0,9)))
+        btree.nodes[9]=Node(\
+                KeySet((66,88)),\
+                PointerSet((0,0,0)))
+        key = 12
 
-        expected_output = Index([]) # Not provided
+
+        expected_output = Index([Node()]*13)
+        expected_output.nodes[0] = Node(\
+                KeySet((7, 27)),\
+                PointerSet((1,2,3)))
+        expected_output.nodes[2] = Node(\
+                KeySet((9,-1)),\
+                PointerSet((4,5,0)))
+        expected_output.nodes[3]=Node(\
+                KeySet((66,-1)),\
+                PointerSet((7,8,0)))
+        expected_output.nodes[6]=Node(\
+                KeySet((11,11)),\
+                PointerSet((0,0,90))) # Dummy data for test
+        expected_output.nodes[7]=Node(\
+                KeySet((7,-1)),\
+                PointerSet((0,0,8)))
+        expected_output.nodes[8]=Node(\
+                KeySet((9,12)),\
+                PointerSet((0,0,10)))
+        expected_output.nodes[10]=Node(\
+                KeySet((27,-1)),\
+                PointerSet((0,0,11)))
+        expected_output.nodes[11]=Node(\
+                KeySet((66,88)),\
+                PointerSet((0,0,0)))
 
         self.assertEqual( expected_output, ImplementMe.InsertIntoIndex( btree, key ) )
 
 
 # Insertion causes splits that propagates at least three times
+# Note that only the path that should be affected has correct data (testing complexity)
+# Linearisation forces copy of some nodes to new addresses
 class TestCase07(unittest.TestCase):
     def test_insertion(self):
-        btree = Index([]) # not provided
-        key = 87
+        btree = Index([Node()]*13)
+        btree.nodes[0] = Node(\
+                KeySet((7, 99)),\
+                PointerSet((1,2,0)))
+        btree.nodes[2]=Node(\
+                KeySet((27,66)),\
+                PointerSet((7,8,9)))
+        btree.nodes[7]=Node(\
+                KeySet((7,9)),\
+                PointerSet((0,0,8)))
+        btree.nodes[8]=Node(\
+                KeySet((27,-1)),\
+                PointerSet((0,0,9)))
+        btree.nodes[9]=Node(\
+                KeySet((66,88)),\
+                PointerSet((0,0,0)))
+        key = 12
 
-        expected_output = Index([]) # Not provided
+        expected_output = Index([Node()]*40)
+        expected_output.nodes[0] = Node(\
+                KeySet((27, -1)),\
+                PointerSet((1,2,0)))
+        expected_output.nodes[1] = Node(\
+                KeySet((7, -1)),\
+                PointerSet((4,5,0)))
+        expected_output.nodes[2] = Node(\
+                KeySet((99, -1)),\
+                PointerSet((7,8,0)))
+        expected_output.nodes[5] = Node(\
+                KeySet((9,-1)),\
+                PointerSet((16,17,0)))
+        expected_output.nodes[7]=Node(\
+                KeySet((66,-1)),\
+                PointerSet((22,23,0)))
+        expected_output.nodes[16]=Node(\
+                KeySet((7,-1)),\
+                PointerSet((0,0,17)))
+        expected_output.nodes[17]=Node(\
+                KeySet((9,12)),\
+                PointerSet((0,0,22)))
+        expected_output.nodes[22]=Node(\
+                KeySet((27,-1)),\
+                PointerSet((0,0,23)))
+        expected_output.nodes[23]=Node(\
+                KeySet((66,88)),\
+                PointerSet((0,0,0)))
 
         self.assertEqual( expected_output, ImplementMe.InsertIntoIndex( btree, key ) )
 
 
-# Lookup key outside range of tree's keys
+# Boundary case: lookup smallest key in tree
+# Fake data in last node to test complexity
 class TestCase08(unittest.TestCase):
     def test_lookup(self):
+        btree = Index([Node()]*4)
+        btree.nodes[0] = Node(\
+                KeySet((42, 66)),\
+                PointerSet((1,2,3)))
+        btree.nodes[1] = Node(\
+                KeySet((9,-1)),\
+                PointerSet((0,0,2)))
+        btree.nodes[2]=Node(\
+                KeySet((42,-1)),\
+                PointerSet((0,0,3)))
+        btree.nodes[3]=Node(\
+                KeySet((66,7)),\
+                PointerSet((0,0,0)))
+        key = 9
+
+        expected_output = True
+
+        self.assertEqual( expected_output, ImplementMe.LookupKeyInIndex( btree, key ) )
+
+
+# Boundary case: lookup largest key in tree
+# Fake data in first node to test complexity
+class TestCase09(unittest.TestCase):
+    def test_lookup(self):
+        btree = Index([Node()]*4)
+        btree.nodes[0] = Node(\
+                KeySet((42, 66)),\
+                PointerSet((1,2,3)))
+        btree.nodes[1] = Node(\
+                KeySet((7,99)),\
+                PointerSet((0,0,2)))
+        btree.nodes[2]=Node(\
+                KeySet((42,-1)),\
+                PointerSet((0,0,3)))
+        btree.nodes[3]=Node(\
+                KeySet((66,87)),\
+                PointerSet((0,0,0)))
+        key = 42
+
+        expected_output = True
+
+        self.assertEqual( expected_output, ImplementMe.LookupKeyInIndex( btree, key ) )
+
+
+
+
+# Lookup key outside range of tree's keys
+# Fake data in middle leaf to test complexity
+class TestCase10(unittest.TestCase):
+    def test_lookup(self):
         btree = Index([Node()]*1)
-        btree.nodes[ 0 ] = Node(\
-            KeySet((42, 99)),\
-            PointerSet((0,0,0)))
+        btree.nodes[0] = Node(\
+                KeySet((42, 66)),\
+                PointerSet((1,2,3)))
+        btree.nodes[1] = Node(\
+                KeySet((9,-1)),\
+                PointerSet((0,0,2)))
+        btree.nodes[2]=Node(\
+                KeySet((7,-1)),\
+                PointerSet((0,0,3)))
+        btree.nodes[3]=Node(\
+                KeySet((66,99)),\
+                PointerSet((0,0,0)))
         key = 7
 
         expected_output = False
@@ -166,53 +315,34 @@ class TestCase08(unittest.TestCase):
 
 
 # Lookup key within tree's range but not in tree
-class TestCase09(unittest.TestCase):
+# Fake data in one leaf to test complexity
+class TestCase11(unittest.TestCase):
     def test_lookup(self):
         btree = Index([Node()]*1)
-        btree.nodes[ 0 ] = Node(\
-            KeySet((42, 99)),\
-            PointerSet((0,0,0)))
-        key = 66
+        btree.nodes[0] = Node(\
+                KeySet((42, 66)),\
+                PointerSet((1,2,3)))
+        btree.nodes[1] = Node(\
+                KeySet((7,-1)),\
+                PointerSet((0,0,2)))
+        btree.nodes[2]=Node(\
+                KeySet((42,-1)),\
+                PointerSet((0,0,3)))
+        btree.nodes[3]=Node(\
+                KeySet((66,9)),\
+                PointerSet((0,0,0)))
+        key = 9
 
         expected_output = False
 
         self.assertEqual( expected_output, ImplementMe.LookupKeyInIndex( btree, key ) )
-
-
-# Boundary case: lookup smallest key in tree
-class TestCase10(unittest.TestCase):
-    def test_lookup(self):
-        btree = Index([Node()]*1)
-        btree.nodes[ 0 ] = Node(\
-            KeySet((42, 99)),\
-            PointerSet((0,0,0)))
-        key = 42
-
-        expected_output = True
-
-        self.assertEqual( expected_output, ImplementMe.LookupKeyInIndex( btree, key ) )
-
-
-# Boundary case: lookup largest key in tree
-class TestCase11(unittest.TestCase):
-    def test_lookup(self):
-        btree = Index([Node()]*1)
-        btree.nodes[ 0 ] = Node(\
-            KeySet((42, 99)),\
-            PointerSet((0,0,0)))
-        key = 99
-
-        expected_output = True
-
-        self.assertEqual( expected_output, ImplementMe.LookupKeyInIndex( btree, key ) )
-
 
 # Lookup key strictly within the tree's range
 class TestCase12(unittest.TestCase):
     def test_lookup(self):
         btree = Index([Node()]*4)
         btree.nodes[0] = Node(\
-                KeySet((42, 66)),\
+                KeySet((41, 66)),\
                 PointerSet((1,2,3)))
         btree.nodes[1] = Node(\
                 KeySet((7,-1)),\
@@ -231,6 +361,7 @@ class TestCase12(unittest.TestCase):
 
 
 # Range query fully contained in one leaf node
+# Fake data in other node to test complexity
 class TestCase13(unittest.TestCase):
     def test_range(self):
         btree = Index([Node()]*4)
@@ -238,7 +369,7 @@ class TestCase13(unittest.TestCase):
                 KeySet((42, 66)),\
                 PointerSet((1,2,3)))
         btree.nodes[1] = Node(\
-                KeySet((7,-1)),\
+                KeySet((7,68)),\
                 PointerSet((0,0,2)))
         btree.nodes[2]=Node(\
                 KeySet((42,-1)),\
@@ -249,12 +380,13 @@ class TestCase13(unittest.TestCase):
         lower_bound = 66
         upper_bound = 87
 
-        expected_output = [66,]
+        expected_output = [66]
 
         self.assertEqual( expected_output, ImplementMe.RangeSearchInIndex( btree, lower_bound, upper_bound ) )
 
 
-# Range query half-open to the left 
+# Range query half-open to the left
+# Fake data in one node to test complexity.
 class TestCase14(unittest.TestCase):
     def test_range(self):
         btree = Index([Node()]*4)
@@ -268,17 +400,18 @@ class TestCase14(unittest.TestCase):
                 KeySet((42,-1)),\
                 PointerSet((0,0,3)))
         btree.nodes[3]=Node(\
-                KeySet((66,99)),\
+                KeySet((66,9)),\
                 PointerSet((0,0,0)))
         lower_bound = 0
         upper_bound = 42
 
-        expected_output = [7,]
+        expected_output = [7]
 
         self.assertEqual( expected_output, ImplementMe.RangeSearchInIndex( btree, lower_bound, upper_bound ) )
 
 
 # Range query half-open to the right 
+# Fake data in one node to test complexity
 class TestCase15(unittest.TestCase):
     def test_range(self):
         btree = Index([Node()]*4)
@@ -286,7 +419,7 @@ class TestCase15(unittest.TestCase):
                 KeySet((42, 66)),\
                 PointerSet((1,2,3)))
         btree.nodes[1] = Node(\
-                KeySet((7,-1)),\
+                KeySet((7,68)),\
                 PointerSet((0,0,2)))
         btree.nodes[2]=Node(\
                 KeySet((42,-1)),\
@@ -294,15 +427,16 @@ class TestCase15(unittest.TestCase):
         btree.nodes[3]=Node(\
                 KeySet((66,87)),\
                 PointerSet((0,0,0)))
-        lower_bound = 7
+        lower_bound = 42
         upper_bound = 99
 
-        expected_output = [7,42,66,87,]
+        expected_output = [9,42,66,87,]
 
         self.assertEqual( expected_output, ImplementMe.RangeSearchInIndex( btree, lower_bound, upper_bound ) )
 
 
-# Range query with matching upper and lower bound 
+# Range query with matching upper and lower bound
+# Key not in tree but found as fake data in a different node to test complexity
 class TestCase16(unittest.TestCase):
     def test_range(self):
         btree = Index([Node()]*4)
@@ -316,7 +450,7 @@ class TestCase16(unittest.TestCase):
                 KeySet((42,-1)),\
                 PointerSet((0,0,3)))
         btree.nodes[3]=Node(\
-                KeySet((66,87)),\
+                KeySet((66,7)),\
                 PointerSet((0,0,0)))
         lower_bound = 7
         upper_bound = 7
@@ -327,6 +461,7 @@ class TestCase16(unittest.TestCase):
 
 
 # Multi-leaf range query in middle of tree
+# Fake data in first node to test complexity
 class TestCase17(unittest.TestCase):
     def test_range(self):
         btree = Index([Node()]*4)
@@ -334,7 +469,7 @@ class TestCase17(unittest.TestCase):
                 KeySet((42, 66)),\
                 PointerSet((1,2,3)))
         btree.nodes[1] = Node(\
-                KeySet((7,-1)),\
+                KeySet((68,-1)),\
                 PointerSet((0,0,2)))
         btree.nodes[2]=Node(\
                 KeySet((42,-1)),\
