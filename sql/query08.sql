63e3929ba47e5727281955051ad7a1cdda9268b3
Sean Chester <schester@uvic.ca>
2022-06-24 11:53:35 -0700

    🎉 First summer query

--- sql/query08.sql
+++ sql/query08.sql
@@ -1,9 +1,10 @@
--- Note: Aspects of this are *very* tricky
--- Retrieve the display name of all users who have
--- never posted a post that has been linked by another post
--- ordered ascending
--- 1.1 marks: <8 operators
--- 1.0 marks: <10 operators
+-- Retrieve the fifteen counties with the largest 2016 vote imbalance,
+-- with their vote counts and states, restricted to counties with at least 10000 votes
+-- Hint: Use pq to measure variance/imbalance in this question,
+-- where p is the probability of voting democrat and q, republican.
+-- 1.1 marks: <11 operators
+-- 1.0 marks: <12 operators
+-- 0.9 marks: <15 operators
 -- 0.8 marks: correct answer
 
 -- Replace this comment line with the actual query
