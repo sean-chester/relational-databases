334b8ee2e485ca54ab9312fac28631fb10f0f2e0
Sean Chester <schester@uvic.ca>
2022-07-05 13:51:39 -0700

    🚑 Fix result for and scoring for query 9
    Logical error pointed out by a student after class yesterday

--- sql/query09.sql
+++ sql/query09.sql
@@ -2,9 +2,9 @@
 -- employed at least 7.5% of the state's 2019 population,
 -- ordered by the total payroll for that industry
 -- in that state.
--- 1.1 marks: <13 operators
--- 1.0 marks: <15 operators
--- 0.9 marks: <20 operators
+-- 1.1 marks: <26 operators
+-- 1.0 marks: <30 operators
+-- 0.9 marks: <35 operators
 -- 0.8 marks: correct answer
 
 
