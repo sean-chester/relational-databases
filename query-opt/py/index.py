ba3edc64f9deee8da04e5a6d3b51671252019345
Sean Chester <schester@uvic.ca>
2022-07-20 14:02:24 -0700

    ♻️ Simplify design of Node  and Index classes

--- query-opt/py/index.py
+++ query-opt/py/index.py
@@ -1,22 +1,5 @@
-# Definition of a Linearised B+-tree index class with fan-out 3
-# The keys are unsigned (i.e., non-negative) integers.
-#
-# This is a *linearised* tree, i.e., it is implemented as an array rather
-# than with pointers. As you have seen in previous classes, one can
-# implement a binary tree of height *h* with an array of length *2^h-1* as follows:
-#   * the root (if it exists) is at index 0
-#   * the left child of the node at index i is at 2i + 1
-#   * the right child of the node at index i is at 2i + 2
-# This corresponds to a breadth-first search of a full binary tree.
-#
-# In this assignment, you will use a fan-out of three, i.e., a ternary tree.
-# We can apply the same logic, and represent a ternary tree of height *h*
-# with an array of length *(1-3^h)/-2* as follows:
-#   * the root (if it exists) is at index 0
-#   * the left child of the node at index i is at 3i + 1
-#   * the middle child of the node at index i is 3i + 2
-#   * the right child of the node at index i is at 3i + 3
-# This corresponds to a breadth-first search of a full ternary tree.
+# Definition of a B+-tree index class with fan-out 3
+# The keys are integers.
 #
 # This B+-tree class does not have any (non-builtin) member functions,
 # but all member variables are public. You will implement the functionality
@@ -25,19 +8,15 @@
 from node import *
 from functools import reduce
 
-# A B+-tree index is thus just an ordered list of nodes. Array position
-# implies position in the tree. The length of the list should be exactly
-# *3^h*, where *h* is the height of the tree. The last nodes may very well be
-# full of nothing but sentinel values (-1 for keys and 0 for pointers).
 class Index:
-    nodes = []
-    def __init__(self, nodes):
-        self.nodes = nodes
+    root = Node()
+    def __init__(self, root = Node()):
+        self.root = root
     def __str__(self):
-        return str(self.nodes)
+        return str(self.root)
     def __repr__(self):
         return str(self)
     def __hash__(self):
-        return reduce(lambda x,y: x^y, [hash(x) for x in self.nodes])
+        return hash(root)
     def __eq__(self, other):
-        return self.nodes == other.nodes
+        return self.root == other.root
--- query-opt/py/node.py
+++ query-opt/py/node.py
@@ -1,5 +1,5 @@
 # Definition of a B+-tree node class with fan-out 3.
-# The keys are unsigned (i.e., non-negative) integers.
+# The keys are integers.
 #
 # This file will not be submitted; so, you are encouraged
 # not to make functional changes to it.
@@ -7,50 +7,51 @@
 
 # Arbitrarily chosen prime number for use in hash functions
 class Constants:
-    arbitrary_prime = 7879
+    a_prime = 7879
 
 # Defines the set of keys in a ternary B+-tree. Observe that there are always
-# two such keys, though some may hold the sentinel value of -1.
+# two such keys, though some may hold the sentinel value of None.
 # The only member variable is a 2-tuple of integer values. Order is important.
 class KeySet:
-    keys = (-1,-1)
-    def __init__(self, keys):
+    keys = [None]*2
+    def __init__(self, keys = [None]*2):
         self.keys = keys
     def __str__(self):
         return str(self.keys)
     def __repr__(self):
         return str(self)
     def __hash__(self):
-        return ( keys[ 0 ] * Constants.arbitrary_prime ) \
-        ^ ( keys[ 1 ] * Constants.arbitrary_prime )
+        if keys[ 0 ] is None:
+            return 0
+        return reduce(lambda x, y: x^y, [x * Constants.a_prime for x in keys if x is not None]) 
     def __eq__(self, other):
         return self.keys == other.keys
 
-# Defines the set of pointers in a ternary B+-tree. These should point to array
-# indices at which the children can be found, or use the 0 sentinel value in the
-# i'th positio iff there is no (i+1)'st child. Order, again, is important.
+# Defines the set of pointers in a ternary B+-tree. These should be references
+# to Node objects at which the children can be found, or use the None sentinel value in the
+# i'th position iff there is no (i+1)'st child. Order, again, is important.
 class PointerSet:
-    pointers = (0,0,0)
-    def __init__(self, pointers):
+    pointers = [None]*3
+    def __init__(self, pointers = [None]*3):
         self.pointers = pointers
     def __str__(self):
         return str(self.pointers)
     def __repr__(self):
         return str(self)
     def __hash__(self):
-        return ( pointers[ 0 ] * Constants.arbitrary_prime ) \
-        ^ ( pointers[ 1 ] * Constants.arbitrary_prime ) \
-        ^ ( pointers[ 2 ] * Constants.arbitrary_prime )
+        if pointers[ 0 ] is None:
+            return 0
+        return reduce(lambda x, y: x^y, [hash(x) * Constants.a_prime for x in pointers if x is not None]) 
     def __eq__(self, other):
         return self.pointers == other.pointers
 
 # A B+-tree node consists of a set of keys and pointers. If this is a leaf
-# node, all pointers should be sentinel values (0). If this is a directory
-# node, only those pointers that correspond to child sub-trees should be non-zero.
+# node, all pointers should be sentinel values [None]*3. If this is a directory
+# node, only those pointers that correspond to child sub-trees should be non-None.
 class Node:
-    keys = KeySet((-1,-1))
-    pointers = PointerSet((0,0,0))
-    def __init__(self, keys = KeySet((-1,-1)), pointers = PointerSet((0,0,0))):
+    keys = KeySet()
+    pointers = PointerSet()
+    def __init__(self, keys = KeySet(), pointers = PointerSet()):
         self.keys = keys 
         self.pointers = pointers
     def __str__(self):
