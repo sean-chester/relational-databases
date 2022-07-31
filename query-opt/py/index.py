9882caa07611f50063cf30dc8316d451ceebe0dd
Sean Chester <schester@uvic.ca>
2022-07-30 23:28:07 -0700

    🚑 Fix buggy default Index constructor
    Same error as with Node/KeySet/PointerSet earlier with using mutable types as default values

--- query-opt/py/index.py
+++ query-opt/py/index.py
@@ -6,14 +6,12 @@
 # in another (implementation) file.
 
 from node import *
-from functools import reduce
 
 class Index:
-    root = Node()
     NUM_KEYS = Node.get_num_keys()
     FAN_OUT = Node.get_fan_out()
-    def __init__(self, root = Node()):
-        self.root = root
+    def __init__(self, root = None):
+        self.root = root if root else Node()
     def __str__(self):
         return str(self.root)
     def __repr__(self):
