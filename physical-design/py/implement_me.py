ccef9e857d0f5d87d329fd0856818ada151b6a21
Sean Chester <schester@uvic.ca>
2022-11-19 17:29:21 -0800

    🌱 Revise comments in implementation file and tests for Fall 2022
    Update comments for new assignment description that now builds from a log file and populate the first two test cases.

--- physical-design/py/implement_me.py
+++ physical-design/py/implement_me.py
@@ -1,36 +1,41 @@
 # Implementation of B+-tree functionality.
 
 from index import *
+from log import *
 
 # You should implement all of the static functions declared
 # in the ImplementMe class and submit this (and only this!) file.
 class ImplementMe:
 
-    # Returns a B+-tree obtained by inserting a key into a pre-existing
-    # B+-tree index if the key is not already there. If it already exists,
-    # the return value is equivalent to the original, input tree.
+    # Returns a B+-tree obtained by replaying the REDO log file.
+    # Unseen values should be inserted;
+    # previously seen values should be deleted and re-inserted.
+    # Values that were never committed should never be inserted.
+    # @precondition: the log file will not try to add an existing key
     #
-    # Complexity: Guaranteed to be asymptotically linear in the height of the tree
+    # Complexity: Each operation is guaranteed to be asymptotically
+    # linear in the height, h, of the tree, O(h).
     # Because the tree is balanced, it is also asymptotically logarithmic in the
-    # number of keys that already exist in the index.
+    # number of keys, n, that already exist in the index, i.e., O(h) = O(lg n).
+    # The entire insertion of a log of length L should be O(L lg n)
     @staticmethod
-    def InsertIntoIndex( index, key ):
-        return index
+    def from_log( log ):
+        return Index()
 
-    # Returns a boolean that indicates whether a given key
-    # is found among the leaves of a B+-tree index.
+    # Returns a list of keys visited by a lookup query to ascertain
+    # whether a given key is found among the leaves of a B+-tree index.
     #
     # Complexity: Guaranteed not to touch more nodes than the
     # height of the tree
     @staticmethod
-    def LookupKeyInIndex( index, key ):
-        return False
+    def lookup( index, key ):
+        return []
 
-    # Returns a list of keys in a B+-tree index within the half-open
-    # interval [lower_bound, upper_bound)
+    # Returns a list of keys visited by a range query in a B+-tree index
+    # within the closed interval [lower_bound, upper_bound]
     #
     # Complexity: Guaranteed not to touch more nodes than the height
     # of the tree and the number of leaves overlapping the interval.
     @staticmethod
-    def RangeSearchInIndex( index, lower_bound, upper_bound ):
+    def range( index, lower_bound, upper_bound ):
         return []
