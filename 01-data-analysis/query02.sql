b42cba90b734596a499011abc49b99bc970ec4b2
Sean Chester <schester@uvic.ca>
2023-05-29 08:30:34 -0700

    Merge main into new-solns

--- 01-data-analysis/query02.sql
+++ 01-data-analysis/query02.sql
@@ -1,19 +1,8 @@
--- Retrieve the total payrolls for all counties that had a population of
--- less than 500 in 2011. Ensure you don't miss any counties.
--- Order the results by descending total payrolls.
--- Hint: you may find the COALESCE function useful.
--- 1.1 marks: <7 operators
--- 1.0 marks: <8 operators
--- 0.9 marks: <11 operators
--- 0.8 marks: correct answer
+-- Order by total payroll all states
+-- that have fewer than 50 counties
+-- and at least ten industries of payroll data
+-- 1.02 marks: <11 operators
+-- 1.00 marks: <12 operators
+-- 0.90 marks: <14 operators
+-- 0.80 marks: correct answer
 
-SELECT `County`.`name` AS `County`, COALESCE(SUM(`payroll`), 0) AS `Total Payroll`
-FROM `CountyPopulation`
-  JOIN `County`
-    ON (`County`.`fips` = `CountyPopulation`.`county`)
-  LEFT OUTER JOIN `CountyIndustries`
-    ON (`County`.`fips` = `CountyIndustries`.`county`)
-WHERE `year` = 2011
-  AND `population` < 500
-GROUP BY `County`.`fips`
-ORDER BY `Total Payroll` DESC;
--- 01-data-analysis/query08.sql
+++ 01-data-analysis/query08.sql
@@ -1,21 +1,8 @@
--- Retrieve alphabetically the abbreviations of all states in which
--- every county has a (relative) difference between
--- male and female populations strictly within 10%.
--- 1.1 marks: <9 operators
--- 1.0 marks: <11 operators
--- 0.9 marks: <14 operators
--- 0.8 marks: correct answer
-
-SELECT `State`.`abbr` AS `State`
-FROM `State`
-WHERE `id` NOT IN
-(SELECT `County`.`state`
-FROM `GenderBreakdown` AS `Boys`
-  JOIN `GenderBreakdown` AS `Girls`
-    ON (`Boys`.`county` = `Girls`.`county`
-    AND `Boys`.`gender` = 'male'
-    AND `Girls`.`gender` = 'female')
-  JOIN `County`
-    ON (`Boys`.`county` = `County`.`fips`)
-WHERE ABS(`Boys`.`population` - `Girls`.`population`) / (`Boys`.`population` + `Girls`.`population`) >= .1 )
-ORDER BY `abbr`;
+-- Retrieve alphabetically the abbreviations of the states
+-- in which one can find the ten counties that had the
+-- largest (absolute) increase in employed persons
+-- between 2008 and 2016.
+-- 1.02 marks: <11 operators
+-- 1.00 marks: <12 operators
+-- 0.90 marks: <14 operators
+-- 0.80 marks: correct answer
