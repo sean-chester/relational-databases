f6de9b6abbf7cdaf8027546b36f9a4004c5116fb
Sean Chester <schester@uvic.ca>
2023-05-31 21:09:06 -0700

    Add additional tie-break attributes to Q6 and Q9
    Sort keys were not functional keys; so, it was possible that the correct query could produce non-matching results on different systems

--- 01-data-analysis/query09.sql
+++ 01-data-analysis/query09.sql
@@ -1,5 +1,6 @@
 -- Show all counties ordered by their total
 -- number of employees across all industries
+-- Tie-break with the county id in ascending order.
 -- 1.1 marks: <5 operators
 -- 1.0 marks: <6 operators
 -- 0.8 marks: correct answer
