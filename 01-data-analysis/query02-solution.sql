990b669b9c2506a92d6f8db674bb91a4afc577bc
Sean Chester <schester@uvic.ca>
2023-06-01 14:54:10 -0700

    Clarify wording of Q2 due to incorrect implication on county counts

--- 01-data-analysis/query02-solution.sql
+++ 01-data-analysis/query02-solution.sql
@@ -1,5 +1,5 @@
 -- Order by total payroll all states
--- that have fewer than 50 counties
+-- that have fewer than 50 counties with payroll data
 -- and at least ten industries of payroll data
 -- 1.02 marks: <11 operators
 -- 1.00 marks: <12 operators
