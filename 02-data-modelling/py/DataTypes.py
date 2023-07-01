6755822f76bf5bc018f8ed69351c2b7f68f2343d
Sean Chester <schester@uvic.ca>
2023-07-01 16:06:48 -0700

    Fix syntax error in class definitions

--- 02-data-modelling/py/DataTypes.py
+++ 02-data-modelling/py/DataTypes.py
@@ -1,13 +1,13 @@
 # Sum type to group together a table name and a list of
 # attributes contained therein
-def Attributes:
+class Attributes:
     def __init__( self, table_name, attributes ):
         self.table_name = table_name
         self.attributes = attributes
 
 # Sum type representing a referential integrity policy
 # as a database operation and a policy for handling violations
-def RefIntegrityPolicy:
+class RefIntegrityPolicy:
     def __init__( self, operation, policy ):
     	assert operation == "UPDATE" or operation == "DELETE" or operation == "INSERT", "Invalid operation"
     	assert policy == "CASCADE" or operation == "REJECT" or operation == "SET NULL", "Invalid policy"
