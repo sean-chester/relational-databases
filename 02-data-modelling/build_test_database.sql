285e6d307d37c3d3160ec6f366214246cafaf5bc
Sean Chester <schester@uvic.ca>
2023-07-30 09:21:19 -0700

    Release a few more table configurations...
    ...to come closer to the set of released tests and decrease assignment difficulty a little bit

--- 02-data-modelling/build_test_database.sql
+++ 02-data-modelling/build_test_database.sql
@@ -47,6 +47,84 @@ CREATE TABLE S6( `v` INT AUTO_INCREMENT
 	               REFERENCES `R6`(`x`)
 	           );
 
+CREATE TABLE R7( `x` INT AUTO_INCREMENT
+	           , `y` INT DEFAULT 7
+	           , `z` INT
+	           , PRIMARY KEY(`x`, `y`)
+	           );
+CREATE TABLE S7( `a` INT AUTO_INCREMENT
+	           , `b` INT
+	           , `c` INT
+	           , PRIMARY KEY(`a`, `b`, `c`)
+	           , FOREIGN KEY(`b`, `c`)
+	               REFERENCES `R7`(`x`, `y`)
+	           );
+
+CREATE TABLE R8( `x` INT AUTO_INCREMENT
+	           , `y` INT DEFAULT 8
+	           , `z` INT
+	           , PRIMARY KEY(`x`, `y`)
+	           );
+CREATE TABLE S8( `a` INT AUTO_INCREMENT
+	           , `b` INT
+	           , `c` INT
+	           , PRIMARY KEY(`a`)
+	           , FOREIGN KEY(`b`, `c`)
+	               REFERENCES `R8`(`x`, `y`)
+	           );
+
+CREATE TABLE R9( `x` INT AUTO_INCREMENT
+	           , `y` INT DEFAULT 9
+	           , `z` INT
+	           , PRIMARY KEY(`x`, `y`)
+	           );
+CREATE TABLE S9( `a` INT AUTO_INCREMENT
+	           , `b` INT
+	           , `c` INT
+	           , PRIMARY KEY(`a`)
+	           , FOREIGN KEY(`b`, `c`)
+	               REFERENCES `R9`(`x`, `y`)
+	           );
+
+
+-- Tables for confirmReferentialIntegrity()
+
+CREATE TABLE R11( `x` INT AUTO_INCREMENT
+	            , `y` INT
+	            , `z` INT
+	            , PRIMARY KEY(`x`)
+	            );
+CREATE TABLE S11( `a` INT AUTO_INCREMENT
+	            , `b` INT
+	            , `c` INT
+	            , PRIMARY KEY(`a`)
+	            , FOREIGN KEY(`c`)
+	                REFERENCES `R11`(`x`)
+	                ON DELETE RESTRICT
+	            );
+
+
+-- Tables for confirmFunctionalDependency()
+
+CREATE TABLE R16( `x` INT AUTO_INCREMENT
+	            , `y` INT
+	            , `z` INT
+	            , PRIMARY KEY(`x`)
+	            );
+
+CREATE TABLE R18( `x` INT AUTO_INCREMENT
+	            , `y` INT
+	            , `z` INT
+	            , PRIMARY KEY(`x`)
+	            );
+CREATE TABLE S18( `a` INT AUTO_INCREMENT
+	            , `b` INT
+	            , `c` INT
+	            , PRIMARY KEY(`a`)
+	            , FOREIGN KEY(`c`)
+	                REFERENCES `R18`(`x`)
+	            );
+
 DROP USER IF EXISTS 'student'@'localhost';
 CREATE USER IF NOT EXISTS 'student'@'localhost' IDENTIFIED BY 'stud3nt';
 GRANT SELECT, INSERT, UPDATE, DELETE, REFERENCES ON `assignment2`.* TO 'student'@'localhost';
